8229b4dd858de920f3fea036ea24baa78505e736
venkat <vikkii89@gmail.com>
2016-02-07 16:32:38 -0800

    times table app

--- TimesTable_IOS/ViewController.swift
+++ TimesTable_IOS/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    
 
+    @IBOutlet weak var slideVal: UISlider!
+
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        print (slideVal)
+        table.reloadData()
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +31,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+        
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+         let cell  = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(slideVal.value * 20)
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        return cell
+        
+    }
+    
 
 
 }
